2d842b43a1583f038429897900520d19ddb6c828
R V Ajinald <113900308+Ajinald@users.noreply.github.com>
2023-12-19 11:44:35 +0530

    Add files via upload

---
+++ Mars Exploration.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int countCharacters(const char *s) {
+    int count = 0;
+
+    for (int i = 0; s[i] != '\0'; i += 3) {
+        if (s[i] != 'S') {
+            count++;
+        }
+        if (s[i + 1] != 'O') {
+            count++;
+        }
+        if (s[i + 2] != 'S') {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+   
+    const char *inputString = "SOSSPSSQSSOR";
+
+    int result = countCharacters(inputString);
+
+    printf("Number of characters are: %d\n", result);
+
+    return 0;
+}
